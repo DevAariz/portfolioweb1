7d8b3622a94999fef75059af4a6525a96aca02c7
Aariz Khan <muhammadaarizkhansocial@gmail.com>
2024-07-07 16:43:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text",{
+    strings:["Java Developer", "Web Developer","Computer Science Student"],
+    typeSpeed : 10,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+    })
+    
